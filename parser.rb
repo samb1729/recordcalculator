07ac27d53d114f121181f0b9323c96c1cc0266f2
Sam Broughton <sam@26th-zerk.co.uk>
2014-06-13 19:31:28 +0100

    Calculate day, week, month records in ruby version

--- parser.rb
+++ parser.rb
@@ -69,20 +69,7 @@ def record(gap, updates)
   pairs.last
 end
 
-Benchmark.bm do |b|
-  b.report("Generate pairs 50 times: ") {
-    50.times do
-      time_pairs(86400, updates)
-    end
-  }
-
-  b.report("Generate records 50 times:") {
-    [86400, 86400 * 7, 86400 * 31].each do |time|
-      50.times do record(time, updates).inspect end
-    end
-  }
-end
-
-[86400, 86400 * 7, 86400 * 31].each do |time|
-  puts record(time, updates).inspect
+[1, 7, 31].each do |time|
+  r = record(time * 86400, updates)
+  puts "#{time} day record xp: #{r.last}"
 end
