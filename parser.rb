c1a75f6c8e96ae54a39d7f865912a95771d97774
Sam Broughton <sam@26th-zerk.co.uk>
2014-06-13 16:33:19 +0100

    Use Benchmark instead of `time` n

--- parser.rb
+++ parser.rb
@@ -1,3 +1,5 @@
+require 'benchmark'
+
 f = open("stats")
 lines = f.read.lines.map(&:chomp)
 f.close
@@ -63,6 +65,14 @@ def record(gap, updates)
   }
 
   pairs.last
+end
+
+Benchmark.bm do |b|
+  b.report("run 50 times:") {
+    [86400, 86400 * 7, 86400 * 31].each do |time|
+      50.times do record(time, updates).inspect end
+    end
+  }
 end
 
 [86400, 86400 * 7, 86400 * 31].each do |time|
